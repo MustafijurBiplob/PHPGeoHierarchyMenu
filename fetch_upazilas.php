085bd932a01e06fee5b4e8a47be5bcd1f1c69df7
MustafijurBiplob <89678553+MustafijurBiplob@users.noreply.github.com>
2023-12-29 10:35:46 +0600

    Update fetch_upazilas.php
    PDO

--- fetch_upazilas.php
+++ fetch_upazilas.php
@@ -5,23 +5,30 @@ $password = ""; // Enter your MySQL password here
 $dbname = "blood";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['district_id'])) {
-    $conn = new mysqli($servername, $username, $password, $dbname);
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+        $districtId = filter_var($_POST['district_id'], FILTER_SANITIZE_NUMBER_INT);
+
+        $sql = "SELECT * FROM upazilas WHERE district_id = :districtId";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':districtId', $districtId, PDO::PARAM_INT);
+        $stmt->execute();
+
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        $options = '<option value="">Select Upazila</option>';
+        foreach ($result as $row) {
+            $options .= '<option value="' . $row['id'] . '">' . $row['upazila_name'] . '</option>';
+        }
+
+        echo $options;
+    } catch (PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+    } finally {
+        $conn = null; // Close the connection
     }
-
-    $districtId = $_POST['district_id'];
-    $sql = "SELECT * FROM upazilas WHERE district_id = $districtId";
-    $result = $conn->query($sql);
-
-    $options = '<option value="">Select Upazila</option>';
-    while ($row = $result->fetch_assoc()) {
-        $options .= '<option value="' . $row['id'] . '">' . $row['upazila_name'] . '</option>';
-    }
-
-    echo $options;
-    $conn->close();
 } else {
     echo "Invalid request";
 }
