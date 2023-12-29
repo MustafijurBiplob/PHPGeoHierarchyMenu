6a07dde8ed638caa72ff2d9008c07c6a1afdf8cd
MustafijurBiplob <89678553+MustafijurBiplob@users.noreply.github.com>
2023-12-29 10:17:22 +0600

    Update fetch_districts.php
    It's important to sanitize input and use prepared statements to prevent SQL

--- fetch_districts.php
+++ fetch_districts.php
@@ -11,16 +11,33 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['division_id'])) {
         die("Connection failed: " . $conn->connect_error);
     }
 
-    $divisionId = $_POST['division_id'];
-    $sql = "SELECT * FROM districts WHERE division_id = $divisionId";
-    $result = $conn->query($sql);
+    // Sanitize input
+    $divisionId = $conn->real_escape_string($_POST['division_id']);
+
+    // Use prepared statement to prevent SQL injection
+    $sql = "SELECT * FROM districts WHERE division_id = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $divisionId);
+    $stmt->execute();
+
+    $result = $stmt->get_result();
 
     $options = '<option value="">Select District</option>';
+    
+    // Store district IDs to ensure uniqueness
+    $selectedDistricts = array();
+
     while ($row = $result->fetch_assoc()) {
-        $options .= '<option value="' . $row['id'] . '">' . $row['district_name'] . '</option>';
+        // Check against the array to ensure uniqueness
+        if (!in_array($row['id'], $selectedDistricts)) {
+            $options .= '<option value="' . $row['id'] . '">' . $row['district_name'] . '</option>';
+            $selectedDistricts[] = $row['id'];
+        }
     }
 
     echo $options;
+
+    $stmt->close();
     $conn->close();
 } else {
     echo "Invalid request";
