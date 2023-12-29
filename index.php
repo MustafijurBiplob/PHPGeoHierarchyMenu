fcd542e98604c323c549a447fa4c00d5950d542f
MustafijurBiplob <89678553+MustafijurBiplob@users.noreply.github.com>
2023-12-29 10:44:14 +0600

    Update index.php
    updated

--- index.php
+++ index.php
@@ -1,19 +1,54 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dropdowns with Bootstrap</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="js/dropdowns.js"></script>
+    <script>
+        $(document).ready(function() {
+            // Fetch districts based on selected division
+            $('#division').change(function() {
+                var divisionId = $(this).val();
+
+                $.ajax({
+                    type: "POST",
+                    url: "fetch_districts.php",
+                    data: { division_id: divisionId },
+                    success: function(response) {
+                        $('#district').html(response);
+                        $('#upazila').html('<option value="">Select Upazila</option>');
+                    },
+                    error: function(xhr, status, error) {
+                        console.error(xhr.responseText);
+                    }
+                });
+            });
+
+            // Fetch upazilas based on selected district
+            $('#district').change(function() {
+                var districtId = $(this).val();
+
+                $.ajax({
+                    type: "POST",
+                    url: "fetch_upazilas.php",
+                    data: { district_id: districtId },
+                    success: function(response) {
+                        $('#upazila').html(response);
+                    },
+                    error: function(xhr, status, error) {
+                        console.error(xhr.responseText);
+                    }
+                });
+            });
+        });
+    </script>
 </head>
 <body>
     <div class="container mt-5">
         <div class="form-group">
             <label for="division">Division:</label>
             <select id="division" class="form-control">
-                <!-- Options will be populated dynamically -->
+                <?php include 'fetch_divisions.php'; ?>
             </select>
         </div>
 
@@ -35,7 +70,16 @@
     <script>
         // Fetch divisions on page load
         $(document).ready(function() {
-            fetchDivisions();
+            $.ajax({
+                type: "GET",
+                url: "fetch_divisions.php",
+                success: function(response) {
+                    $('#division').html(response);
+                },
+                error: function(xhr, status, error) {
+                    console.error(xhr.responseText);
+                }
+            });
         });
     </script>
 
